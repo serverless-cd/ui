c2897ff3607d225ab4ef5be87744ec322f0bf938
heimanba <371510756@qq.com>
2023-01-31 14:15:37 +0800

    mse arc 架构图删除

--- packages/mse-arc-ui/.fatherrc.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  // more father 4 config: https://github.com/umijs/father-next/blob/master/docs/config.md
-  umd: {
-    name: 'mseArc',
-    autoprefixer: {
-      overrideBrowserslist: ['> 1%', 'last 2 versions'],
-    },
-  },
-};

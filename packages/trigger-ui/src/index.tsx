303a2852b0cd5afb9b5d98b6c4734962e3553c9a
wb-fc932227 <wb-fc932227@alibaba-inc.com>
2023-02-27 09:33:27 +0800

    trigger 组件修改

--- packages/trigger-ui/src/index.tsx
+++ packages/trigger-ui/src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { TriggersProps } from './types';
 import Trigger from './TriggerContent';
 import Preview from './TriggerPreview';
+import { valuesFormat as formatStrict, normalValuesFormat as formatNormal } from './utils/util';
+
+export const valuesFormat = (values) => formatStrict(values);
+export const normalValuesFormat = (values) => formatNormal(values);
 
 class TriggerUi extends React.Component<TriggersProps> {
   triggerUiRef: any;
